dfab064dd87f6ed143fa44fe7b6f80f60f8edd04
Acrokx <acroxmen@gmail.com>
2025-03-25 20:05:18 -0500

    Delete VideoJuego directory

--- VideoJuego/conjuro.py
+++
@@ -1,5 +0,0 @@
-class Conjuro_
-    
-        def __init__(self, nombre, poder):
-            self.nombre = nombre
-            self.poder = poder
--- VideoJuego/espada.py
+++
@@ -1,5 +0,0 @@
-class Espada:
-    def __init__(self, nombre, danio):
-        self.nombre = nombre
-        self.danio = danio
-       
